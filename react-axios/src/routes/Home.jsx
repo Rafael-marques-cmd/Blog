498ec104d073fe85c08874867c3a3c59078e6e7a
Rafael-marques-cmd <marquesrafael1243@gmail.com>
2026-03-16 15:52:26 -0300

    começando projeto

---
+++ react-axios/src/routes/Home.jsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+
+const Home = () => {
+    const [posts, setPosts] = useState([]);
+
+    const getPosts = async() => {
+      try {  
+        const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+
+        const data = response.data;
+
+        setPosts(data);
+      } catch (error) {
+        console.log(error);
+      }
+
+
+    };
+
+    useEffect(() => {
+getPosts()
+    
+    },[])
+
+
+
+  return (
+    <div>
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? <p>Carregando...</p> : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+          </div>
+        ))
+      )}
+    </div>
+  )
+}
+
+export default Home;
